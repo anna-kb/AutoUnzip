905cae099595c019e2cda68332eb60fd78d74166
anna-kb <129875026+anna-kb@users.noreply.github.com>
2023-05-03 11:51:29 -0600

    prompt for replace or skip

--- AutoUnzip/MainWindow.xaml.cs
+++ AutoUnzip/MainWindow.xaml.cs
@@ -7,7 +7,9 @@ using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading.Tasks;
-
+using System.Runtime.InteropServices;
+using System.Windows.Shapes;
+using System.Media;
 
 namespace AutoUnzip
 {
@@ -69,11 +71,19 @@ namespace AutoUnzip
             DataContext = this;
             InitializeComponent();
 
-            sourceFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads";
-            destinationFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Documents\Electronic Arts\The Sims 3\Mods\Packages";
-            progressValue = 0;
+            _sourceFolder = "C:\\Users\\Meepi\\Desktop\\delete\\sims3";
+            //Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads";
+            _destinationFolder = "C:\\Users\\Meepi\\Desktop\\delete\\sims3\\testDestination";
+            //Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Documents\Electronic Arts\The Sims 3\Mods\Packages";
+            _progressValue = 0;
+            _progressString = "";
         }
 
+
+        /*
+         * Handles the Browse button's Click event. Depending on the sender of the event,
+         * update the source or destination folder path.
+         * */
         private void browseBtn_Click(object sender, RoutedEventArgs e)
         {
             WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
@@ -96,6 +106,10 @@ namespace AutoUnzip
             }
         }
 
+        /*
+         * Handles the submit button click. Confirm with user that files will be deleted,
+         * then start processing.
+         * */
         private void submitBtn_Click(object sender, RoutedEventArgs e)
         {
             if (processing)
@@ -109,6 +123,10 @@ namespace AutoUnzip
             }
         }
 
+        /*
+         * Start a Task on a new background thread in other to display
+         * visual elements to the UI, namely the progress, which increments with each file processed.
+         */
         private async void StartProcessing()
         {
             processing = true;
@@ -135,6 +153,10 @@ namespace AutoUnzip
             progressString = "All Done!";
         }
 
+
+        /*
+         * Deletes zip files after processing all files if user checked  the box.
+         */
         private void DeleteAllFiles(List<string> files)
         {
             foreach(var file in files)
@@ -159,23 +181,27 @@ namespace AutoUnzip
             }
         }
 
+        /*
+         * First, check if the destination contains files within the zip files. Prompt the user to replace or skip in that case.
+         * Then, start a new Process with the overwrite argument (if user skipped, then assume always overwrite)
+         */
         private void ProcessFile(string file)
         {
             //Check if destination already contains file
-            string possibleFile = destinationFolder + file.Substring(file.LastIndexOf('\\') );
-            System.Diagnostics.Debug.WriteLine(possibleFile);
-            string overWriteMode = "";
-            
-            if(File.Exists(possibleFile))
-            {
-                bool b= PromptReplace(file);
+            List<string> matches = CheckForMatches(file);
 
-                overWriteMode = b ? "-aoa" : "-aos";
+            if(matches.Count >0)
+            {
+                bool replace = ReplacePrompt(matches);
+                if (!replace)
+                {
+                    return;
+                }
             }
 
             var startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.FileName = @"C:\Program Files\7-Zip\7z.exe";
-            startInfo.Arguments = $"x \"{file}\" -o\"{destinationFolder}\" {overWriteMode}";
+            startInfo.Arguments = $"x \"{file}\" -o\"{destinationFolder}\" -aoa";
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.UseShellExecute = false;
             startInfo.CreateNoWindow = true;
@@ -187,10 +213,89 @@ namespace AutoUnzip
                 process.Start();
                 process.WaitForExit();
             }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Encountered error in ProcessFile: {ex.Message}");
+            }
             finally
             {
                 process.Dispose();
             }
+        }
+
+        /*
+         * Create a process that outputs the files in the .zip, and pass it out the parsing function.
+         */
+        private List<string> CheckForMatches(string file)
+        {
+            List<string> fileMatches = new List<string>();
+
+            var startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = @"C:\Program Files\7-Zip\7z.exe";
+            startInfo.Arguments = $"l \"{file}\" -o\"{destinationFolder}\"";
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+
+            var process = new System.Diagnostics.Process();
+            process.StartInfo = startInfo;
+            try
+            {
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                List<string> fileNames = ParseOutput(output);
+
+                foreach(string potentialMatch in fileNames)
+                {
+                    string checkString = destinationFolder + $"\\{potentialMatch}";
+                    System.Diagnostics.Debug.WriteLine(checkString);
+                    if (File.Exists(checkString))
+                    {
+                        fileMatches.Add(potentialMatch);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Encountered error in CheckForMatches: {ex.Message}");
+            }
+            finally
+            {            
+                process.Dispose();
+            }
+
+            return fileMatches;
+        }
+
+        /*
+         * Parses the output from 7zip l command to get file names. 
+         */
+        private List<string> ParseOutput(string output)
+        {
+            List<string> files = new List<string>();
+
+            string delimiter = "-------------------";
+            string[] lines = output.Split('\n');
+            int indexOfDelimiter = Array.FindIndex(lines, (l => l.Contains(delimiter)));
+            int indexOfNameColumn = lines[indexOfDelimiter - 1].IndexOf("Name");
+
+            for(int i = indexOfDelimiter+1; i < lines.Length; i++)
+            {
+                if (lines[i].StartsWith(delimiter))  // we've reached the end of the file output.
+                    break;
+
+                if (lines[i].Substring(0, indexOfNameColumn).Contains("D...."))  // this refers to a folder, and not a file.
+                    continue;
+
+                string fileName = lines[i].Substring(indexOfNameColumn).Trim();
+                files.Add(fileName);
+            }
+
+            return files;
         }
 
         private void WarnUser()
@@ -204,9 +309,18 @@ namespace AutoUnzip
 
         }
 
-        private bool PromptReplace(string file)
+        private bool ReplacePrompt(List<string> files)
         {
-            MessageBoxResult result = MessageBox.Show($"The file, {file}, already exists in the destination folder. Would you like to replace it?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            string listString = "";
+
+            for(int i =0; i < files.Count;i++)
+            {
+                listString += files[i];
+                if (i != files.Count - 1)
+                    listString += "\n";
+            }
+            MessageBoxResult result = MessageBox.Show($"The following files exist in the destination folder. Do you want to replace them? \n \n {listString}", 
+                "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
             if (result == MessageBoxResult.Yes)
                 return true;
